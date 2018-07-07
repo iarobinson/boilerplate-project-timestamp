fef93edefe0442693d3c148c1166116d64fdb450
Ian Robinson <ian@ianrobinson.net>
2018-07-07 13:21:55 -0700

    Local host is now serving api calls

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 // Importing required modules for Node
-
-var express = require("express");
 var bodyParser = require("body-parser");
-var cors = require('cors');
+var cors = require("cors");
+var express = require("express");
 
 // express instance creation
-
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// functionality goes here
+app.get('/dateValues/:dateVal', function(req, res, next) {
+  var dateVal = req.params.dateVal;
+  console.log('huh?');
+  res.json({ unix: dateVal });
+});
+
+// Set the server to listen for functions on 3000 port
 app.listen(3000, function() {
-  console.log('now it is working....');
+  console.log("Node Functioning");
 });
